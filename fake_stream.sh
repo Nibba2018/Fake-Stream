5db4bc2b475a172276ad95d7a3231a54a1da001f
Soham Biswas <sohambiswas41@gmail.com>
2020-11-10 18:11:36 +0530

    added kernel module checks

--- fake_stream.sh
+++ fake_stream.sh
@@ -1,4 +1,7 @@
-INPUT=$1
+if ! sudo modprobe v4l2loopback; then
+   echo "Unable to probe kernel module."
+   exit ;
+fi
 
 WEBCAMS=$(ls /dev/video*)
 echo $WEBCAMS
@@ -8,8 +11,10 @@ WEBCAM=$(grep $ID <<< $WEBCAMS)
 while [ true ]; do
    read -t 1 -n 1
    if [ $? = 0 ] ; then
+      # cleanup
+      sudo modprobe -r v4l2loopback
       exit ;
    else
-	ffmpeg -re -i $INPUT -map 0:v -f v4l2 $WEBCAM
+	   ffmpeg -re -i $1 -map 0:v -f v4l2 $WEBCAM
    fi
 done
