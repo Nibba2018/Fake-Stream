3d838ddc022c6d8b4581cbaf8e22316022d8b98f
whokilleddb <debjeetbanerjee06042001@gmail.com>
2021-03-26 04:34:41 +0530

    Graceful Exit, more bug fixes, help menu and documentaion

--- fake_stream.sh
+++ fake_stream.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Capture SIGINT and exit gracefully
+trap exitgracefully SIGINT
+
 #Specify Color Schemes
 NONE='\033[00m'
 RED='\033[01;31m'
 GREEN='\033[01;32m'
 YELLOW='\033[01;33m'
+BLUE='\033[01;34m'
 PURPLE='\033[01;35m'
 CYAN='\033[01;36m'
 WHITE='\033[01;37m'
@@ -18,16 +22,49 @@ help()
 $0 [OPTIONS/FILENAME]
 
 OPTIONS :
---source : Stream Desktop
+-h --help : Show This Menu 
+--source  : Stream Desktop
 
 FILENAME: Path to Video/Image
 	"""
 }
+
+
+askYesNo()
+{
+        QUESTION=$1
+        DEFAULT=$2
+        if [ "$DEFAULT" = true ]; then
+                OPTIONS="[Y/n]"
+                DEFAULT="y"
+            else
+                OPTIONS="[y/N]"
+                DEFAULT="n"
+        fi
+        read -p "$QUESTION $OPTIONS " -n 1 -s -r INPUT
+        INPUT=${INPUT:-${DEFAULT}}
+        echo ${INPUT}
+        if [[ "$INPUT" =~ ^[yY]$ ]]; then
+            ANSWER=true
+        else
+            ANSWER=false
+        fi
+}
+
 # function to start fake stream
 streamvideo()
 {
       echo -e "${GREEN}${BOLD}[+] Playing $VIDEO On The Stream Pointed By $WEBCAM"
       ffmpeg -stream_loop -1 -re -i /tmp/video -vcodec rawvideo -threads 0 -f v4l2 $WEBCAM
+      # ffmpeg : The program which would enable us to stream our video as the webcam feed
+      # -stream_loop -1 : This dictates how many times the video should be looped. Assigning it the negative value of -1 makes it loop infinitely so that it keeps on playing till we close our program.
+      # -re : This specifies the program to read input at native frame rate
+      # -i : Specifies the input file name. It is followed by the path to the video file we want to stream
+      # -vcodec : This specifies the video codec, aka the stream handling.
+      # rawvideo : This tell ffmpeg to use the Raw video demuxer. This demuxer allows one to read raw video data.
+      # -threads : The number of threads to be used. Usually setting it to 0 is considered be optimal.
+      # -f : This flag is used force the format of input/output file. In our case, we are forcing output to v4l2 format
+      # $WEBCAM : It specifies the functioning video devices as selected by the user.
 }
 
 streamdesktop()
@@ -57,9 +94,6 @@ banner()
 
 cleanup()
 {
-	clear
-	#Display Banner
-	banner
 	rm -f /tmp/video 2>/dev/null
 	if [[ $# -eq 0 ]] ; then
 	    echo -e "${RED}${BOLD}No argument supplied${NONE}"
@@ -69,8 +103,37 @@ cleanup()
 
 }
 
+exitgracefully()
+{
+	echo -e "${RED}${BOLD}[-] Received SIGINT"
+	echo -e "${PURPLE}${BOLD}[+] Cleaning Up"
+	while [ true ]; do
+		echo -e "${CYAN}[-] Turn Off Video First !${NONE}${BOLD}"
+		askYesNo "[-] Did You Turn Off Video ? " true
+		DOIT=$ANSWER
+		if [ "$DOIT" = true ]; then
+		    out=$(sudo modprobe -r v4l2loopback 2>&1 >/dev/null)
+		    if [[ ! -z "$out" ]];then
+		    	echo -e "${BOLD}${YELLOW}[-] Your Video Stream Might Freeze As The Module Was Still In Use When Removed "
+		    	echo -e "${BLUE}${BOLD}[+] Restarting Your Video Should Fix It !"
+		    fi
+		    rm -f /tmp/video
+			echo -e "${GREEN}${BOLD}[+] Clean Up Done !"
+			exit
+		else
+			echo -e "${BOLD}${RED}${UNDERLINE}[+] Turn Off Your Video Feed First !"
+		fi
+		
+	done
+}
+
 # Initial Cleanup
 cleanup $@
+
+if [ "$@" = "-h" ]||[ "$@" = "--help" ]; then
+ 	help
+ 	exit
+fi
 
 # Probing Kernel Modules
 if ! sudo modprobe v4l2loopback card_label="My Fake Webcam" exclusive_caps=1; then
@@ -78,25 +141,26 @@ if ! sudo modprobe v4l2loopback card_label="My Fake Webcam" exclusive_caps=1; th
    exit ;
 fi
 
-echo -e "${CYAN}[+] Available Video Devices : "
+# Clear Screen
+clear
 
+# Display Banner
+banner
+
+# Select Video Device
+echo -e "${CYAN}[+] Available Video Devices : "
 WEBCAMS=$(ls -1 /dev/video*)
 echo -e "${PURPLE}$WEBCAMS${NONE}"
-
 read -p "[+] Choose Webcam ID (last digit) : " ID
 WEBCAM=$(grep $ID <<< $WEBCAMS)
 
+# Created A Soft Link To The Video File
 VIDEO="$@"
 PWD=$(pwd)/$VIDEO
 ln -s $PWD /tmp/video
 
 while [ true ]; do
-   read -t 1 -n 1
-   if [ $? = 0 ] ; then
-      # cleanup
-      sudo modprobe --remove v4l2loopback
-      exit ;
-   elif [ "$VIDEO" = "--source" ]; then
+	if [ "$VIDEO" = "--source" ]; then
    		streamdesktop
    else	
 		streamvideo
